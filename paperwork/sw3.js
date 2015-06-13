69bb1d41f1c6572d14570bc8a1324f0fef3649cd
Steven Hermans <steven166@live.nl>
2015-06-13 19:27:31 +0200

    update sw

--- paperwork/sw3.js
+++ paperwork/sw3.js
@@ -1,18 +1,29 @@
 importScripts('js/serviceworker-cache-polyfill.js');
 
-// example usage:
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
+    // pre cache a load of stuff:
     event.waitUntil(
-        caches.open('demo-cache').then(function(cache) {
-            return cache.put('/', new Response("From the cache!"));
+        cachesPolyfill.open('myapp-static-v1').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/activities/',
+                '/js/_app.js',
+                '/js/_init.js',
+                '/images/',
+                '../../shared/images/icons/',
+                '../../shared/css/icons/',
+                '../../shared/css/paper-bundle.css',
+                '../../shared/js/jquery.min.js',
+                '../../shared/js/paper-bundle.js'
+            ]);
         })
-    );
+    )
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || new Response("Nothing in the cache for this request");
+        cachesPolyfill.match(event.request).then(function (response) {
+            return response || fetch(event.request);
         })
     );
 });
