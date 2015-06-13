c269c1faa4c4df4d8d03bef1f8b1083cc1b7e0d7
Steven Hermans <steven166@live.nl>
2015-06-13 19:08:44 +0200

    add sw.js

--- paperwork/sw.js
+++ paperwork/sw.js
@@ -1,17 +1,17 @@
-
-var CACHE_NAME = 'paperwork-api-cache-v1';
-var urlsToCache = [
-    '/',
-    '/activities/',
-    '/js/_app.js',
-    '/js/_init.js',
-    '/images/',
-    '../../shared/images/icons/',
-    '../../shared/css/icons/',
-    '../../shared/css/paper-bundle.css',
-    '../../shared/js/jquery.min.js',
-    '../../shared/js/paper-bundle.js'
-];
+//
+//var CACHE_NAME = 'paperwork-api-cache-v1';
+//var urlsToCache = [
+//    '/',
+//    '/activities/',
+//    '/js/_app.js',
+//    '/js/_init.js',
+//    '/images/',
+//    '../../shared/images/icons/',
+//    '../../shared/css/icons/',
+//    '../../shared/css/paper-bundle.css',
+//    '../../shared/js/jquery.min.js',
+//    '../../shared/js/paper-bundle.js'
+//];
 
 //self.addEventListener('install', function(event) {
 //    // Perform install steps
@@ -70,17 +70,17 @@ var urlsToCache = [
 
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
-//console.log("SW startup");
-//
-//self.addEventListener('install', function(event) {
-//    console.log("SW installed");
-//});
-//
-//self.addEventListener('activate', function(event) {
-//    console.log("SW activated");
-//});
-//
-//self.addEventListener('fetch', function(event) {
-//    console.log("Caught a fetch!");
-//    event.respondWith(new Response("Hello world!"));
-//});
+console.log("SW startup");
+
+self.addEventListener('install', function(event) {
+    console.log("SW installed");
+});
+
+self.addEventListener('activate', function(event) {
+    console.log("SW activated");
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log("Caught a fetch!");
+    event.respondWith(new Response("Hello world!"));
+});
